ba35ac22951f3c35da507f9f04ed5f8e490450ed
HeflerDev <hheflerdevelopment@gmail.com>
2020-11-10 14:10:04 -0300

    assets/gathering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import Phaser from 'phaser';
 
-const main = () => {
+import Menu from '../src/scenes/menu';
 
-};
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 520,
+    parent: 'phaser-container',
+    dom: {
+        createContainer: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y : 300 },
+            debug: false
+        }
+    },
+    scene: [Menu]
+}
+
+export default new Phaser.Game(config);
---
+++ src/scenes/menu.js
@@ -0,0 +1,9 @@
+import Phaser from 'phaser';
+
+console.log(Phaser)
+
+export default class Menu extends Phaser.Scene {
+    constructor() {
+        super('menu');
+    }
+}
